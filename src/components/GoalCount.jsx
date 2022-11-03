9242b880f98fd41632e716146bb95b12a49efbb1
MIchael  Kmiecik <93628363+kmiecik013@users.noreply.github.com>
2022-11-02 20:31:25 -0600

    addition of more stuff

--- src/components/GoalCount.jsx
+++ src/components/GoalCount.jsx
@@ -1,13 +1,38 @@
-
+import Axios from "axios";
+import { useState } from "react"
+import "../App.css"
 
 
 export default function GoalCount() {
+
+  const url = "https://statsapi.web.nhl.com/api/v1/people/8471214/stats?stats=statsSingleSeason&season=20222023"
+  
+  const [goals, setGoals] = useState("")
+
+  const oviCount = () => {
+    Axios.get(url)
+    .then((res) =>{
+      setGoals(894 - (res.data.stats[0].splits[0].stat.goals + 780))
+    
+      return setGoals(894 - (res.data.stats[0].splits[0].stat.goals + 780))
+
+    console.log(res.data.stats[0].splits[0].stat.goals)
+
+  })
+}
 
 
   return (
     <div className="goal-count">
     
-      700
+   
+
+    
+
+    <p className="goal-count">{oviCount(goals)}{goals}</p>
+
+
+
 
       
 
