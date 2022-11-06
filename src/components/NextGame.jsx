02a144688dcc08e1c24d5b7d0ac75228465ba27d
MIchael  Kmiecik <93628363+kmiecik013@users.noreply.github.com>
2022-11-06 01:47:35 -0600

    added the future dates of wsh games

--- src/components/NextGame.jsx
+++ src/components/NextGame.jsx
@@ -6,12 +6,11 @@ import "../App.css"
 
 export default function NextGame() {
 
-  const url = "https://statsapi.web.nhl.com/api/v1/schedule?teamId=15&startDate=2022-11-05&endDate=2023-05-01"
-  
-const d =  new Date()
-let day = d.getDate(); 
 
-console.log("here", d)
+const d =  new Date()
+let currentDay = d.getFullYear() + "-" + (d.getMonth()+1) + "-" + 0+d.getDate(); 
+  const url = `https://statsapi.web.nhl.com/api/v1/schedule?teamId=15&startDate=${currentDay}&endDate=2023-05-01`
+  
 
   const [nextGame, setNextGame] = useState("")
   const [date, setDate] =useState("")
@@ -21,29 +20,63 @@ const whenNextGame = () => {
   Axios.get(url)
   .then((res) => {
 
-    setNextGame(res.data.dates[1].games[0].teams.away.team.name)
+    setNextGame(res.data.dates[0].games[0].teams.away.team.name)
     
-      return setNextGame(res.data.dates[1].games[0].teams.away.team.name)
+      return setNextGame(res.data.dates[0].games[0].teams.away.team.name)
 
-    console.log(res.data.dates[1].games[0].teams.away.team.name)
+    console.log(res.data.dates[0].games[0].teams.away.team.name)
   })
 }
 
 const dateNextGame = () => {
   Axios.get(url)
   .then((res) => {
-    setDate(res.data.dates[1].date)
-    
-      return setDate(res.data.dates[1].date)
 
-      console.log(res.data.dates[1].date)
+
+    let apiDate = (res.data.dates[0].date)
+  
+
+    console.log("here4", apiDate)
+
+    const months = ["Janrary", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+    
+      let numMonth = apiDate.toString().split("-")
+
+      let specificMonth = Number(numMonth[1]);
+      let specificDate = Number(numMonth[2]);
+      let specificYear = Number(numMonth[0]);
+
+
+
+    const getMonth=() => {
+
+      for (let i = 0; i < months.length; i++) {
+        return months[specificMonth-1]
+      }
+    }
+        console.log("here6", getMonth())
+
+    const getTotalDate= () => {
+      return getMonth() + " " + numMonth[2] + " " + numMonth[0] 
+    }
+     
+ console.log("here5", numMonth)
+
+    setDate(getTotalDate())
+    
+      return setDate(getTotalDate())
+
+      console.log(res.data)
   })
+
+
 }
 
 
 return (
     <div>
-    <p className="next-game"> Ovi's next chance to score is againts the {whenNextGame(nextGame)}{nextGame} on {dateNextGame(date)}{date} </p>
+    <p className="next-game"> Ovi's next chance to score is against the <span className="nhl-team"> {whenNextGame(nextGame)}{nextGame}</span> on {dateNextGame(date)}{date} </p>
 
      
     </div>
